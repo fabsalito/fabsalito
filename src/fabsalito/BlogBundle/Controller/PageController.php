f0098f05c6c55718e37eaf83f4aea035383f820e
fabsalito <fabricio.salinas@gmail.com>
2013-04-18 13:36:18 -0500

    Added tags cloud.- Added lastest comments.-

--- src/fabsalito/BlogBundle/Controller/PageController.php
+++ src/fabsalito/BlogBundle/Controller/PageController.php
@@ -18,4 +18,27 @@ class PageController extends Controller
             'blogs' => $blogs
         ));
     }
+
+    public function sidebarAction()
+    {
+        $em = $this->getDoctrine()
+                   ->getEntityManager();
+
+        $tags = $em->getRepository('fabsalitoBlogBundle:Blog')
+                   ->getTags();
+
+        $tagWeights = $em->getRepository('fabsalitoBlogBundle:Blog')
+                         ->getTagWeights($tags);
+
+        $commentLimit   = $this->container
+                           ->getParameter('fabsalito_blog.comments.latest_comment_limit');
+        
+        $latestComments = $em->getRepository('fabsalitoBlogBundle:Comment')
+                             ->getLatestComments($commentLimit);
+
+        return $this->render('fabsalitoBlogBundle:Page:sidebar.html.twig', array(
+            'latestComments' => $latestComments,
+            'tags' => $tagWeights
+        ));
+    }
 }
